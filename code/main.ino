81bce4289a39e7b4c52a73cfebd0f6ce5a71fb97
Ninja581 <63921487+Ninja581@users.noreply.github.com>
2022-04-24 15:05:46 +0200

    Update main.ino

--- code/main.ino
+++ code/main.ino
@@ -1,9 +1,16 @@
 void setup() {
   // put your setup code here, to run once:
-
+   pinMode(2, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+   readyled();
+}
 
+void readyled(){
+   digitalWrite(2, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(2, LOW);
+  delay(1000); // Wait for 1000 millisecond(s)
 }
